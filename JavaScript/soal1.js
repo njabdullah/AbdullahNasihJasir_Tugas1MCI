7e0e356a119430da86f43ce014e8606f980d321c
Abdullah Nasih Jasir <92930757+njabdullah@users.noreply.github.com>
2023-03-24 21:12:40 +0700

    Create soal1.js

---
+++ JavaScript/soal1.js
@@ -0,0 +1,12 @@
+// Abdullah Nasih Jasir
+
+const TugasMCI1 = function() 
+{
+    console.log("Saya Calon Admin MCI");
+    let time = 3000;
+    setTimeout(function()
+    {
+        console.log("Abdullah Nasih Jasir");
+    }, time);
+};
+TugasMCI1();
