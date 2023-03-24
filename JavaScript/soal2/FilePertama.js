5a5b34de233afc30d9ceca45511d28602586fe61
Abdullah Nasih Jasir <92930757+njabdullah@users.noreply.github.com>
2023-03-24 21:18:37 +0700

    Create FilePertama.js

---
+++ JavaScript/soal2/FilePertama.js
@@ -0,0 +1,24 @@
+  function penjumlahan(x,y)
+  {
+    return x+y;
+  }
+  
+  function pengurangan(x,y)
+  {
+    return x-y;
+  }
+  
+  function pembagian(x,y)
+  {
+    return x/y;
+  }
+
+  function perkalian(x,y) 
+  {
+    return x*y;
+  }
+  
+  module.exports =
+  {
+    penjumlahan, pengurangan, pembagian, perkalian
+  };
